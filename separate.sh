cc0c33341017a74d5782ce2e8e73ced832c38061
Junbeom Kwon <76743210+Junebeomstics@users.noreply.github.com>
2025-01-03 16:26:06 -0600

    Update separate.sh

--- separate.sh
+++ separate.sh
@@ -4,6 +4,7 @@ NCORE=8
 dwi=`jq -r '.dwi' config.json`
 bvals=`jq -r '.bvals' config.json`
 bvecs=`jq -r '.bvecs' config.json`
+target_bval=`jq -r '.bval' config.json`
 folders="nodif dwi"
 
 for FOLDER in $folders
@@ -12,7 +13,7 @@ do
 
 	# separate b0s from sos and sense data
 	if [ ${FOLDER} == "nodif" ]; then
-		dwiextract -bzero ${dwi} -fslgrad ${bvecs} ${bvals} ./${FOLDER}/dwi.nii.gz -export_grad_fsl ./${FOLDER}/dwi.bvecs ./${FOLDER}/dwi.bvals -nthreads $NCORE -force
+		dwiextract ${dwi} -shell ${target_bval} -fslgrad ${bvecs} ${bvals} ./${FOLDER}/dwi.nii.gz -export_grad_fsl ./${FOLDER}/dwi.bvecs ./${FOLDER}/dwi.bvals -nthreads $NCORE -force
 	else
 		dwiextract -no_bzero ${dwi} -fslgrad ${bvecs} ${bvals} ./${FOLDER}/dwi.nii.gz -export_grad_fsl ./${FOLDER}/dwi.bvecs ./${FOLDER}/dwi.bvals -nthreads $NCORE -force
 	fi
