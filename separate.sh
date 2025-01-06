b6098a8fbaf61e85ed1bb058b7aec5382963a1e8
Junbeom Kwon <76743210+Junebeomstics@users.noreply.github.com>
2025-01-06 11:06:42 -0600

    Update separate.sh
    allows multiple shell extraction

--- separate.sh
+++ separate.sh
@@ -13,7 +13,7 @@ do
 
 	# separate b0s from sos and sense data
 	if [ ${FOLDER} == "nodif" ]; then
-		dwiextract ${dwi} -shell ${target_bval} -fslgrad ${bvecs} ${bvals} ./${FOLDER}/dwi.nii.gz -export_grad_fsl ./${FOLDER}/dwi.bvecs ./${FOLDER}/dwi.bvals -nthreads $NCORE -force
+		dwiextract ${dwi} -shells ${target_bval} -fslgrad ${bvecs} ${bvals} ./${FOLDER}/dwi.nii.gz -export_grad_fsl ./${FOLDER}/dwi.bvecs ./${FOLDER}/dwi.bvals -nthreads $NCORE -force
 	else
 		dwiextract -no_bzero ${dwi} -fslgrad ${bvecs} ${bvals} ./${FOLDER}/dwi.nii.gz -export_grad_fsl ./${FOLDER}/dwi.bvecs ./${FOLDER}/dwi.bvals -nthreads $NCORE -force
 	fi
